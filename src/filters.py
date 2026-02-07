326e1b24679b192f0ad5cd8f1e8c1093b4ac051a
Trisha Mae Villareal <villarealtrisha0123@gmail.com>
2026-02-07 18:21:24 +0800

    Added Chromatic Aberration Glitch Effect logic

--- src/filters.py
+++ src/filters.py
@@ -9,3 +9,8 @@ def apply_color_grading(image):
     img_f[:,:,2] *= 1.3   # Boost Red channel
     return np.clip(img_f, 0, 255).astype(np.uint8)
     
+def apply_chromatic_aberration(image, shift=5):
+    b, g, r = cv2.split(image)
+    b = np.roll(b, -shift, axis=1)
+    r = np.roll(r, shift, axis=1)
+    return cv2.merge([b, g, r])
